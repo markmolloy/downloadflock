323e33976cdd023c26eb028106c303ee8641e0e2
Mark Molloy <markmolloy57@gmail.com>
2018-03-04 13:41:43 +0000

    basic site created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,31 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule, MatFormFieldModule, MatGridListModule, MatCheckboxModule, MatToolbarModule, MatMenuModule, MatIconModule} from '@angular/material';
+import { MatInputModule, MatSnackBarModule, MatTableModule, MatProgressSpinnerModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
