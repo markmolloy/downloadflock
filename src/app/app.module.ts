a45666709a57a1c66b3dca43afa7bd01c6ca9f1a
Mark Molloy <markmolloy57@gmail.com>
2018-03-05 22:01:27 +0000

    changed structure
    updated apk too

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatButtonModule, MatFormFieldModule, MatGridListModule, MatCheckboxModule, MatToolbarModule, MatMenuModule, MatIconModule} from '@angular/material';
 import { MatInputModule, MatSnackBarModule, MatTableModule, MatProgressSpinnerModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
 
+import { LandingComponent } from './landing/landing.component';
+import { AllBlogsComponent } from './blog/all-blogs/all-blogs.component';
+
+const appRoutes: Routes = [
+  { path: 'landing', component: LandingComponent },
+  { path: 'blog', component: AllBlogsComponent },
+  { path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full'
+  },
+  { path: '**', component: LandingComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AllBlogsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
